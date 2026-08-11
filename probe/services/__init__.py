0623344790122d17c206e86274df3bfb75ca169a
Grace Mwai <gracejmwai@gmail.com>
2026-08-11 06:46:08 +0300

    fix a typo

--- probe/services/__init__.py
+++ probe/services/__init__.py
@@ -2,11 +2,11 @@ from .battery import BatteryCreate, BatteryUpdate
 
 __all__=[
       "BatteryCreate", "BatteryUpdate"
+       "get_user", "create_user", "delete_user", "authenticate_user"
+]
 from .user import get_user, create_user, delete_user, authenticate_user
 
-__all__ = [
-    "get_user", "create_user", "delete_user", "authenticate_user"
-]
+
 from .device import (
     create_device,
     delete_device,
